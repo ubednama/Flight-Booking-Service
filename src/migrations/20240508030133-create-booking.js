9d921557dd54a88a3bbcb7e14971ba5344d53857
ubednama <shahidkhan.ak18@gmail.com>
2024-05-08 09:08:02 +0530

    added booking model

---
+++ src/migrations/20240508030133-create-booking.js
@@ -0,0 +1,52 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+
+const {Enums} = require('../utils/common')
+const { CONFIRMED, PENDING, CANCELLED, INITIATED } = Enums.STATUS
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Bookings', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      flightId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      status: {
+        type: Sequelize.ENUM,
+        allowNull: false,
+        values: [CONFIRMED, PENDING, CANCELLED, INITIATED],
+        defaultValue: INITIATED
+      },
+      passengers: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        defaultValue: 1
+      },
+      totalCost: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Bookings');
+  }
+};
---
+++ src/utils/common/enums.js
@@ -0,0 +1,18 @@
+const SEAT_TYPE = {
+    BUSINESS: 'business',
+    ECONOMY: 'economy',
+    PREMIUM_ECONOMY: 'premium-economy',
+    FIRST_CLASS: 'first-class'
+}
+
+const STATUS = {
+    CONFIRMED: "confirmed",
+    PENDING: "pending",
+    CANCELLED: "cancelled",
+    INITIATED: "initiated"
+}
+
+module.exports = {
+    SEAT_TYPE,
+    STATUS
+}
